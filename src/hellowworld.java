de2e0e4634a60976ec137da75fa27fd4e8fa63c0
28024 <2802425265@qq.com>
2022-06-15 11:07:44 +0800

    conmitied test local git

---
+++ src/hellowworld.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class hellowworld {
+    public static void main(String[] args) {
+        String a = "11";
+        System.out.println(a);
+        ArrayList a1 = new ArrayList();
+
+
+
+
+    }
+
+}
